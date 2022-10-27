0199ea4d99488a68cf9c0e4e036f5a1b1a24bd59
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-27 23:49:53 +0300

    v2

--- tutorial-app-inclass/src/pages/Home.jsx
+++ tutorial-app-inclass/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
 import axios from "axios";
+import { useState } from "react";
 
 const Home = () => {
+  const [tutorials, setTutorials] = useState([])
 
 const url = " https://tutorials-api-cw.herokuapp.com/api/tutorials";
 
@@ -11,6 +13,8 @@ const url = " https://tutorials-api-cw.herokuapp.com/api/tutorials";
     const data = await axios(url)
     console.log(data);
   };
+  
+  getTutorials()
 
 
   return (
