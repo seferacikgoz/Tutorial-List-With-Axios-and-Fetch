aa97342126ff5f21b5265c5a612515e63d09f80d
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-27 22:50:10 +0300

    Tutorial List With Axios & Fetch

---
+++ tutorial-app-inclass/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import AddTutorial from "../components/AddTutorial";
+import TutorialList from "../components/TutorialList";
+
+
+const Home = () => {
+
+  return (
+    <>
+      <AddTutorial/>
+      <TutorialList/>
+    </>
+  );
+};
+
+export default Home;
