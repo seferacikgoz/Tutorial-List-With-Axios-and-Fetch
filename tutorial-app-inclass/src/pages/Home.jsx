3d873fe01d81ab49d47dab6d255a4d01cece3e65
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-27 23:45:28 +0300

    axios

--- tutorial-app-inclass/src/pages/Home.jsx
+++ tutorial-app-inclass/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
-
+import axios from "axios";
 
 const Home = () => {
+
+const url = " https://tutorials-api-cw.herokuapp.com/api/tutorials";
+
+  const getTutorials = async () => {
+
+    const data = await axios(url)
+    console.log(data);
+  };
+
 
   return (
     <>
